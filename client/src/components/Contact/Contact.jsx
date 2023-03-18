eea6d1c1a5b0cc4c8551b374922b2563822245b7
yoantodorov <todorovyoan075@gmail.com>
2023-03-18 21:30:18 +0200

    Work on contact
    Finished design and almost finished functionality

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,16 +1,38 @@
-import styles from "./Contact.module.scss"
+import { useState } from 'react'
+
+import { ContactForm } from "./ContactForm/ContactForm";
 import { Map } from "./Map/Map";
 
+import styles from "./Contact.module.scss"
+
 export const Contact = () => {
+    const [isLocate, setIsLocate] = useState(false);
+
+    // Set cookie to keep map open
+    const onLocateClick = () => setIsLocate(state => !state);
+
     return (
         <>
             <div className={styles['title-wrapper']}>
                 <h1 className={styles['title']}>Contact Us</h1>
             </div>
 
-            
+            <div className={styles['main-content-wrapper']}>
+                <div className={styles['image-wrapper']}>
+                    <img src="https://images.unsplash.com/photo-1531450664978-9be7f46cb497?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" />
+                </div>
 
-            <Map />
+
+                {
+                    !isLocate 
+                    ?   <ContactForm onLocateClick={onLocateClick} formStyle="form-wrapper"/>
+                    :   <>
+                            <ContactForm onLocateClick={onLocateClick} formStyle="form-map-wrapper"/>
+                            <Map onLocateClick={onLocateClick} />
+                        </>
+                }
+            </div>
+
         </>
     );
 }
---
+++ client/src/components/Contact/ContactForm/ContactForm.jsx
@@ -0,0 +1,96 @@
+import { useState, useEffect } from 'react'
+
+import styles from './ContactForm.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
+export const ContactForm = ({
+    formStyle,
+    onLocateClick,
+}) => {
+    const [formValues, setFormValues] = useState({
+        fullName: '',
+        email: '',
+        message: '',
+        additionalDetails: '',
+    });
+
+    const onChangeHandler = (e) => 
+        setFormValues(state => ({...state, [e.target.name]: e.target.value}));
+        
+    //TODO: Make validation for email and make them necessary
+
+    return (
+        <div className={styles[formStyle]}>
+            <form>
+                <div className={styles['input-item-wrapper']}>
+                    <label htmlFor="fullName" className={styles['input-item-label']}>Full Name *</label>
+                    <input
+                        className={styles['input-item']}
+                        type="text"
+                        name="fullName"
+                        id="fullName"
+                        value={formValues.fullName}
+                        onChange={onChangeHandler}
+                    />
+                </div>
+                <div className={styles['input-item-wrapper']}>
+                    <label htmlFor="email" className={styles['input-item-label']}>Email *</label>
+                    <input
+                        className={styles['input-item']}
+                        type="text"
+                        name="email"
+                        id="email"
+                        value={formValues.email}
+                        onChange={onChangeHandler}
+                    />
+                </div>
+                <div className={styles['input-item-wrapper']}>
+                    <label htmlFor="message" className={styles['input-item-label']}>Message *</label>
+                    <input
+                        className={styles['input-item']}
+                        type="text"
+                        name="message"
+                        id="message"
+                        value={formValues.message}
+                        onChange={onChangeHandler}
+                    />
+                </div>
+                <div className={styles['input-item-wrapper']}>
+                    <label htmlFor="additionalDetails" className={styles['input-item-label']}>Additional Details</label>
+                    <textarea
+                        rows="3"
+                        className={styles['input-item']}
+                        type="text"
+                        name="additionalDetails"
+                        id="additionalDetails"
+                        value={formValues.additionalDetails}
+                        onChange={onChangeHandler}
+                    />
+                </div>
+
+                <input type='submit' className={styles['submit-btn']} />
+            </form>
+            <div className={styles['information-wrapper']}>
+                <h3>Contact Information</h3>
+                <div className={styles['info-item-wrapper']}>
+                    <FontAwesomeIcon className={styles['icon']} icon={faLocationDot} size="lg" />
+                    <p className={styles['info-item']}>Todor Balina, Veliko Tarnovo <br /> +359 81 234 5678</p>
+                </div>
+                <div className={styles['info-item-wrapper']}>
+                    <FontAwesomeIcon className={styles['icon']} icon={faLocationDot} size="lg" />
+                    <p className={styles['info-item']}>President Linclon, Sofia <br /> +359 81 234 5678</p>
+                </div>
+                <div className={styles['info-item-wrapper']}>
+                    <FontAwesomeIcon className={styles['icon']} icon={faLocationDot} size="lg" />
+                    <p className={styles['info-item']}>Ivan Drasov, Varna <br /> +359 81 234 5678</p>
+                </div>
+                <div className={styles['info-item-wrapper']}>
+                    <FontAwesomeIcon className={styles['icon']} icon={faLocationDot} size="lg" />
+                    <p className={styles['info-item']}>Tsarigrad, Ruse <br /> +359 81 234 5678</p>
+                </div>
+                <button type="button" className={styles['map-btn']} onClick={onLocateClick}>Locate Us</button>
+            </div>
+        </div>
+    );
+}
