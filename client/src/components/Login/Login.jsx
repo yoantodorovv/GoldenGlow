ebdc486c72125670ac83eff6b1cf6223852a2ee9
yoantodorov <todorovyoan075@gmail.com>
2023-03-19 08:46:12 +0200

    Finished Login component

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,5 +1,150 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import * as validate from './services/validationService'
+
+import styles from './Login.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faXmark, faArrowLeftLong } from '@fortawesome/free-solid-svg-icons'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+
 export const Login = () => {
+    const [formValues, setFormValues] = useState({
+        email: '',
+        password: '',
+    });
+    const [validateFormValues, setValidateFormValues] = useState({
+        email: '',
+        password: '',
+    });
+    const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+    const [isTouched, setIsTouched] = useState({
+        email: false,
+        password: false,
+    });
+
+    const isTouchedHandler = (e) =>
+        setIsTouched(state => ({ ...state, [e.target.name]: true }));
+
+    useEffect(() => {
+        const validationArr = Array.from(Object.values(validateFormValues)).filter(x => x !== 'true');
+
+        if (validationArr.length > 0) {
+            return setIsBtnDisabled(true)
+        }
+
+        return setIsBtnDisabled(false);
+    }, [validateFormValues.email, validateFormValues.password]);
+
+    const formValuesValidate = {
+        email: validate.email,
+        password: validate.password,
+    }
+
+    const onChangeHandler = (e) => {
+        isTouchedHandler(e);
+
+        return setFormValues(state => ({ ...state, [e.target.name]: e.target.value }));
+    }
+
+
+    const onBlurHandler = (e) => {
+        const validationError = formValuesValidate[e.target.name](e.target.value);
+
+        return setValidateFormValues(state => ({ ...state, [e.target.name]: validationError }))
+    }
+
     return (
-        <h1>Login</h1>
+        <div className={styles['general-wrapper']}>
+            <div className={styles['image-wrapper']}>
+                <img src="https://images.unsplash.com/photo-1562157873-818bc0726f68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80" alt="" />
+            </div>
+            <form className={styles['form']}>
+                <div className={styles['title-wrapper']}>
+                    <h1 className={styles['title']}>Sign In</h1>
+                </div>
+                <div className={styles['input-item-wrapper']}>
+                    <label htmlFor="email" className={styles['input-item-label']}>Email</label>
+                    <input
+                        className={styles['input-item']}
+                        type="text"
+                        name="email"
+                        id="email"
+                        value={formValues.email}
+                        onChange={onChangeHandler}
+                        onBlur={onBlurHandler}
+                    />
+                    {
+                        isTouched.email
+                        ? (
+                            <>
+                                <div className={styles['validate']}>
+                                    {
+                                        validateFormValues.email == 'true'
+                                            ? <FontAwesomeIcon className={styles['icon-check']} icon={faCheck} />
+                                            : <FontAwesomeIcon className={styles['icon-error']} icon={faXmark} />
+                                    }
+                                </div>
+                                {
+                                    validateFormValues.email !== 'true'
+                                        ? <div className={styles['error']}>
+                                            <p>{validateFormValues.email}</p>
+                                        </div>
+                                        : null
+                                }
+                            </>
+                        )
+                        : null
+                    }
+                </div>
+                <div className={styles['input-item-wrapper']}>
+                    <label htmlFor="password" className={styles['input-item-label']}>Password</label>
+                    <input
+                        className={styles['input-item']}
+                        type="password"
+                        name="password"
+                        id="password"
+                        value={formValues.password}
+                        onChange={onChangeHandler}
+                        onBlur={onBlurHandler}
+                    />
+                    {
+                        isTouched.password
+                        ? (
+                            <>
+                                <div className={styles['validate']}>
+                                    {
+                                        validateFormValues.password == 'true'
+                                            ? <FontAwesomeIcon className={styles['icon-check']} icon={faCheck} />
+                                            : <FontAwesomeIcon className={styles['icon-error']} icon={faXmark} />
+                                    }
+                                </div>
+                                {
+                                    validateFormValues.password !== 'true'
+                                        ? <div className={styles['error']}>
+                                            <p>{validateFormValues.password}</p>
+                                        </div>
+                                        : null
+                                }
+                            </>
+                        )
+                        : null
+                    }
+                </div>
+
+                <p className={styles['register-btn-p']}>Don't have an account? <Link to="/register" className={styles['register-btn']}>Sign Up</Link></p>
+
+                <button type='button' className={styles['google-btn']}>
+                    <FontAwesomeIcon icon={faGoogle} className={styles['google-icon']} size="lg" />
+                    Continue with Google
+                </button>
+                <button type='submit' className={isBtnDisabled ? styles['submit-btn-disabled'] : styles['submit-btn']} disabled={isBtnDisabled}>Sign In</button>
+
+                <Link to="/" className={styles['back-btn']}>
+                    <FontAwesomeIcon className={styles['back-btn-icon']} icon={faArrowLeftLong} />
+                    Home
+                </Link>
+            </form>
+        </div>
     );
 }
