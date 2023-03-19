4e057cabfa18656661469156ce290b4f140607a7
yoantodorov <todorovyoan075@gmail.com>
2023-03-19 15:23:40 +0200

    NotFound component finished

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,12 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+
 import styles from './NotFound.module.scss'
 
 export const NotFound = () => {
+    const navigate = useNavigate();
+
+    const onButtonClick = () => {
+        navigate(-1);
+    }
+
     return (
         <div className={styles['wrapper']}>
             <h1>404</h1>
             <h2>Page not found!</h2>
 
-            <button type="button" className={styles['btn']}>Go Back</button>
+            <button onClick={onButtonClick} type="button" className={styles['btn']}>Go Back</button>
         </div>
     );
 }
