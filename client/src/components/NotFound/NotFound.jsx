5c0f824fa728fad16c589b990bac01dac6d03d75
yoantodorov <todorovyoan075@gmail.com>
2023-03-19 13:44:32 +0200

    Changes to notFound component

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,5 +1,12 @@
+import styles from './NotFound.module.scss'
+
 export const NotFound = () => {
     return (
-        <h1>404 Not Found!</h1>
+        <div className={styles['wrapper']}>
+            <h1>404</h1>
+            <h2>Page not found!</h2>
+
+            <button type="button" className={styles['btn']}>Go Back</button>
+        </div>
     );
 }
