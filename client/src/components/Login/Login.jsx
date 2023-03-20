d9d7c268e4d435c0be58f0332f3154efe11b8c0a
yoantodorov <todorovyoan075@gmail.com>
2023-03-20 21:50:04 +0200

    Added a shop password button in Login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -8,7 +8,7 @@ import Swal from 'sweetalert2'
 
 import styles from './Login.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck, faXmark, faArrowLeftLong } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faXmark, faArrowLeftLong, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 import login from '../../../public/images/Login/login.png'
 export const Login = () => {
@@ -25,6 +25,7 @@ export const Login = () => {
         email: false,
         password: false,
     });
+    const [passwordType, setPasswordType] = useState('password');
     const navigate = useNavigate();
 
     const isTouchedHandler = (e) =>
@@ -43,6 +44,14 @@ export const Login = () => {
     const formValuesValidate = {
         email: validate.email,
         password: validate.password,
+    }
+
+    const passwordTypeHandler = () => {
+        if (passwordType === "password") {
+            setPasswordType("text")
+            return;
+        }
+        setPasswordType("password")
     }
 
     const onChangeHandler = (e) => {
@@ -103,7 +112,7 @@ export const Login = () => {
             Swal.fire({
                 title: 'Sign In failed',
                 text: 'Sorry, we were unable to sing you in. Please try again later.',
-                icon: 'error',  
+                icon: 'error',
                 confirmButtonText: 'Go Back',
                 confirmButtonColor: '#6c3d1f',
                 allowEnterKey: true,
@@ -133,66 +142,75 @@ export const Login = () => {
                     />
                     {
                         isTouched.email
-                        ? (
-                            <>
-                                <div className={styles['validate']}>
+                            ? (
+                                <>
+                                    <div className={styles['validate']}>
+                                        {
+                                            validateFormValues.email == 'true'
+                                                ? <FontAwesomeIcon className={styles['icon-check']} icon={faCheck} />
+                                                : <FontAwesomeIcon className={styles['icon-error']} icon={faXmark} />
+                                        }
+                                    </div>
                                     {
-                                        validateFormValues.email == 'true'
-                                            ? <FontAwesomeIcon className={styles['icon-check']} icon={faCheck} />
-                                            : <FontAwesomeIcon className={styles['icon-error']} icon={faXmark} />
+                                        validateFormValues.email !== 'true'
+                                            ? <div className={styles['error']}>
+                                                <p>{validateFormValues.email}</p>
+                                            </div>
+                                            : null
                                     }
-                                </div>
-                                {
-                                    validateFormValues.email !== 'true'
-                                        ? <div className={styles['error']}>
-                                            <p>{validateFormValues.email}</p>
-                                        </div>
-                                        : null
-                                }
-                            </>
-                        )
-                        : null
+                                </>
+                            )
+                            : null
                     }
                 </div>
                 <div className={styles['input-item-wrapper']}>
                     <label htmlFor="password" className={styles['input-item-label']}>Password</label>
-                    <input
-                        className={styles['input-item']}
-                        type="password"
-                        name="password"
-                        id="password"
-                        value={formValues.password}
-                        onChange={onChangeHandler}
-                        onBlur={onBlurHandler}
-                    />
+                    <div className={styles['input-wrapper']}>
+                        <input
+                            className={styles['input-item-pass']}
+                            type={passwordType}
+                            name="password"
+                            id="password"
+                            value={formValues.password}
+                            onChange={onChangeHandler}
+                            onBlur={onBlurHandler}
+                        />
+                        <button type="button" onClick={passwordTypeHandler} className={styles['hide-password']}>
+                            {
+                                passwordType === 'password'
+                                    ? <FontAwesomeIcon icon={faEye} />
+                                    : <FontAwesomeIcon icon={faEyeSlash} />
+                            }
+                        </button>
+                    </div>
                     {
                         isTouched.password
-                        ? (
-                            <>
-                                <div className={styles['validate']}>
+                            ? (
+                                <>
+                                    <div className={styles['validate-password']}>
+                                        {
+                                            validateFormValues.password == 'true'
+                                                ? <FontAwesomeIcon className={styles['icon-check']} icon={faCheck} />
+                                                : <FontAwesomeIcon className={styles['icon-error']} icon={faXmark} />
+                                        }
+                                    </div>
                                     {
-                                        validateFormValues.password == 'true'
-                                            ? <FontAwesomeIcon className={styles['icon-check']} icon={faCheck} />
-                                            : <FontAwesomeIcon className={styles['icon-error']} icon={faXmark} />
+                                        validateFormValues.password !== 'true'
+                                            ? <div className={styles['error']}>
+                                                <p>{validateFormValues.password}</p>
+                                            </div>
+                                            : null
                                     }
-                                </div>
-                                {
-                                    validateFormValues.password !== 'true'
-                                        ? <div className={styles['error']}>
-                                            <p>{validateFormValues.password}</p>
-                                        </div>
-                                        : null
-                                }
-                            </>
-                        )
-                        : null
+                                </>
+                            )
+                            : null
                     }
                 </div>
 
                 <p className={styles['register-btn-p']}>Don't have an account? <Link to="/register" className={styles['register-btn']}>Sign Up</Link></p>
 
-                <button 
-                    type='button' 
+                <button
+                    type='button'
                     onClick={signInWithGoogle}
                     className={styles['google-btn']}
                 >
