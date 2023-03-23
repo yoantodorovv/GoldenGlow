2ddcc6eda030ccf9d72c9e30d4dbe44e7df9b5f2
yoantodorov <todorovyoan075@gmail.com>
2023-03-23 06:22:26 +0200

    Design work on Home page

--- client/src/components/Home/HomeCatalog/HomeCatalog.jsx
+++ client/src/components/Home/HomeCatalog/HomeCatalog.jsx
@@ -1,18 +1,25 @@
 import { HomeCatalogSectionPreview } from './HomeCatalogSectionPreview/HomeCatalogSectionPreview';
-import { Card } from '../../Card/Card'
+import { HeroContainer } from '../../MainMenu/HeroContainer/HeroContainer'
 
+import collectionBoth from '../../../../public/images/Home/collectionBoth.jpeg'
+import collectionMen from '../../../../public/images/Home/collectionMen.jpeg'
+import collectionWomen from '../../../../public/images/Home/collectionWomen.jpeg'
 import styles from './HomeCatalog.module.scss'
 import { Carousel } from '../../Carousel/Carousel';
 
 export const HomeCatalog = () => {
     return (
         <div className={styles['general-wrapper']}>
-            <HomeCatalogSectionPreview title="Most Popular" className={styles['wrapper-element']}>
+            <HomeCatalogSectionPreview title="Most Popular">
                 <Carousel />
             </HomeCatalogSectionPreview>
             <div className={styles['separator']}></div>
-            <HomeCatalogSectionPreview title="New Collection" className={styles['wrapper-element']}>
-                //TODO: Add a box to the whole collection
+            <HomeCatalogSectionPreview title="New Collection">
+                <div className={styles['collection-container']}>
+                    <HeroContainer imageSrc={collectionWomen} containerText="Women" />
+                    <HeroContainer imageSrc={collectionBoth} containerText="Catalog" />
+                    <HeroContainer imageSrc={collectionMen} containerText="Men" />
+                </div>
                 <Carousel />
             </HomeCatalogSectionPreview>
         </div>
