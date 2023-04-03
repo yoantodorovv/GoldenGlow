83ef5d408735abf179be1dbf7eb97ab24c2ef238
yoantodorov <todorovyoan075@gmail.com>
2023-04-03 14:25:08 +0300

    Finished credit card payment method design

--- client/src/components/ShoppingCart/CreditCardForm/CreditCardForm.jsx
+++ client/src/components/ShoppingCart/CreditCardForm/CreditCardForm.jsx
@@ -5,32 +5,77 @@ import styles from './CreditCardForm.module.scss'
 export const CreditCardForm = ({
     displayName
 }) => {
-    const [cardNumber, setCardNumber] = useState('');
+    const [number, setNumber] = useState('');
+    const [name, setName] = useState('');
+    const [expiry, setExpiry] = useState('');
+    const [cvc, setCvc] = useState('');
 
-    const handleCardNumberChange = (e) => {
-        console.log(e.target.value);
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        if (name === 'number') {
+            setNumber(value.replace(/\s/g, '').replace(/(\d{4})/g, '$1 ').trim());
+        } else if (name === 'expiry') {
+            const formattedExpiry = value
+                .replace(/\s/g, '')
+                .replace(/(\d{2})(\d{1,2})/, '$1/$2')
+                .trim();
 
-        setCardNumber(e.target.value);
+            setExpiry(formattedExpiry);
+        } else {
+            eval(`set${name.charAt(0).toUpperCase() + name.slice(1)}('${value}')`);
+        }
     }
 
     return (
         <>
             <div className={styles['payment-information-option']}>
                 <p>Name On Card</p>
-                <h4>{displayName}</h4>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Cardholder Name"
+                    maxLength="50"
+                    onChange={handleInputChange}
+                    value={name}
+                    required
+                />
             </div>
             <div className={styles['payment-information-option']}>
                 <p>Card Number</p>
-                <input type="tel" inputmode="numeric" pattern="[0-9\s]{13,19}" autocomplete="cc-number" maxlength="19" placeholder='1234 5678 1234 5678' value={cardNumber} onChange={handleCardNumberChange} />
+                <input
+                    type="tel"
+                    name="number"
+                    maxLength="19"
+                    placeholder='1234 5678 1234 5678'
+                    value={number}
+                    onChange={handleInputChange}
+                    required
+                />
             </div>
             <div className={styles['payment-information-option-subwrapper']}>
                 <div className={`${styles['payment-information-option']} ${styles['date']}`}>
                     <p>Expiration Date</p>
-                    <input type="tel" pattern="\d*" maxlength="7" placeholder='MM/YY' />
+                    <input
+                        type="text"
+                        name="expiry"
+                        maxLength="5"
+                        placeholder='MM/YY'
+                        value={expiry}
+                        onChange={handleInputChange}
+                        required
+                    />
                 </div>
                 <div className={`${styles['payment-information-option']} ${styles['cvv']}`}>
-                    <p>CVV</p>
-                    <input type="tel" pattern="\d*" maxlength="4" placeholder='CVV' />
+                    <p>CVV / CVC</p>
+                    <input
+                        type="tel"
+                        name="cvc"
+                        maxLength="3"
+                        placeholder='123'
+                        value={cvc}
+                        onChange={handleInputChange}
+                        required
+                    />
                 </div>
             </div>
         </>
