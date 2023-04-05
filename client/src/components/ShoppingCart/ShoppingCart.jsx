04a42c7e684386119bb8c8c1ee2ca7e157f5a981
yoantodorov <todorovyoan075@gmail.com>
2023-04-05 06:54:21 +0300

    Test commit
    Testing wether firebase automatically updates the production stage

--- client/src/components/ShoppingCart/ShoppingCart.jsx
+++ client/src/components/ShoppingCart/ShoppingCart.jsx
@@ -246,7 +246,7 @@ export const ShoppingCart = () => {
                                     onClick={onCheckout}
                                     className={styles['payment-btn']}
                                 >
-                                    Checkout now now
+                                    Checkout
                                 </button>
                             )
                             : (
